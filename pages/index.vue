c2c08a482747e07e22a43a1ef45758f23740500b
GuillermoK1 <guillermojkr@gmail.com>
2024-12-31 19:48:26 +0000

    MVP complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,48 @@
 <template>
-  <UContainer :class="['svg-bg', `bg-${color}-500`, 'flex', 'h-52','justify-between', 'items-center']">
+  <UContainer :class="['svg-bg', `bg-${color}-400`, 'bg-opacity-50', 'grid', 'gap-6', 'py-6', 'grid-cols-2', 'h-auto','justify-between', 'items-center']">
     <UCard>
       <h2>
-        Check out my <a href="https://aboutme-ahl.pages.dev" :class="`text-${color}-500 font-bold`">website</a>
+        <a href="https://aboutme-ahl.pages.dev">Check out my <span :class="`text-${color}-500 font-bold`">website</span></a>
       </h2>
     </UCard>
     <UCard>
       <h2>
-        Check out my <a href="https://github.com/GuillermoK1" :class="`text-${color}-500 font-bold`">GitHub</a> repositories
+        <a href="https://github.com/GuillermoK1">Check out my <span :class="`text-${color}-500 font-bold`">GitHub</span> repositories</a>
       </h2>
     </UCard>
     <UCard>
       <h2>
-        Check out my <a href="https://plasma.nuxt.dev" :class="`text-${color}-500 font-bold`">app</a> built with Nuxt3
+        <a href="https://plasma.nuxt.dev">Check out my <span :class="`text-${color}-500 font-bold`">app</span> built with Nuxt3</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
+      </h2>
+    </UCard>
+    <UCard class="opacity-50">
+      <h2>
+        <a href="https://">Check out my <span :class="`text-${color}-500 font-bold`">placeholder</span> placeholder</a>
       </h2>
     </UCard>
   </UContainer>
